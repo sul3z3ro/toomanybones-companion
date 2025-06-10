1d19cf2bd9fd200751007a7722aa6d705e27ebbc
sul3z3ro <mitsunarikyou@gmail.com>
2025-06-10 15:31:10 +0700

    update version detector

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         {/* ปรับสี Status bar บนมือถือ */}
         <meta name="theme-color" content="#1976d2" />
+        
+        {/* บังคับไม่ให้ cache เพื่อให้อัปเดตเสมอ */}
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+
         {/* (เพิ่ม favicon ถ้ามี) */}
       </Head>
       <body>
